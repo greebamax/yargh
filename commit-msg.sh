e3f7a77721289bb01783dbd6bff0048daa0a2853
Maxim Grib <maxim.grib@adform.com>
2016-06-12 18:32:51 +0300

    Move 'Merge' from whitelisted prefixes, update comment

--- commit-msg.sh
+++ commit-msg.sh
@@ -1,3 +1,4 @@
+
 #!/bin/sh
 #
 # The hook script validates commit messages on each commit.
@@ -11,15 +12,16 @@ function join { local IFS="$1"; shift; echo "$*"; }
 
 # allowed prefixes
 jira_project_prefixes=(AG ND) # list of JIRA projects prefixes
-whitelisted_prefixes=(FIX HOTFIX REFACTORING TYPO TECH TEST Merge) # allowed words which used to determine commit kind
+whitelisted_prefixes=(FIX HOTFIX REFACTORING TYPO TECH TEST) # allowed words which used to determine commit kind
 
 # regex to validate commit message
-commit_regex="^($(join \| ${whitelisted_prefixes[@]})|($(join \| ${jira_project_prefixes[@]})){1}\-[0-9]+){1}[[:blank:]]"
+commit_regex="^(Merge|$(join \| ${whitelisted_prefixes[@]})|($(join \| ${jira_project_prefixes[@]})){1}\-[0-9]+){1}[[:blank:]]"
 
 error_msg="
 ABORTING COMMIT:
-Your commit message is missing either a JIRA Issue ('AG-XXXX' or 'ND-XXXX') or one of allowed prefixes.
+Your commit message is missing either a JIRA Issue or one of allowed prefixes.
 Allowed prefixes: FIX, HOTFIX, REFACTORING, TYPO, TECH or TEST.
+Examples: 'AG-1234 Commit message preceded by a single space' or 'FIX hotfix to prod'
 "
 
 if grep -iqE "$commit_regex" "$1"; then
